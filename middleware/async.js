a44d3510ffdbd416632b5ad69e44ce54b91f8cac
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-04 20:18:16 +0530

    async wrapper function

--- middleware/async.js
+++ middleware/async.js
@@ -1,9 +1,10 @@
-
+// To call functions in try and catch block
+// To avoid writing try and catch for different controllers
 const asyncWrapper =(fun)=>{
 
     return async (req,res,next)=>{
         try{
-            fun(req,res)
+            await fun(req,res)
         }
         catch(err){
             next(err)
